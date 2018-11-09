f0cb3e61e328778491b37f1db993058c2f081064
Alexandra <strekalo@gmail.com>
2018-11-09 23:22:14 +0100

    add content on about page
    Signed-off-by: Alexandra <strekalo@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,14 @@ const Link = styled.span`
 const SecondPage = () => (
   <Layout>
     <h1>About</h1>
-    <p>I am a full-stack developer living in Berlin. I have experience in retail and online-advertising industries.</p>
+    <p>I am a full-stack developer living in Berlin.</p>
+    <p>I have experience with a full range of technologies - from Java and Spring on the backend to JavaScript and React.js on the frontend side 
+      and GoCD, Docker and Kubernetes in the infrastructure area.</p>
+    <p>In the past years I have been working in retail, automotive and online-advertising industries.</p>
+    <p>Recently I was working on a large several year project distributed geografically across several teams. 
+      The architecutre model for this project was Micro Fronteds. 
+      I gave a <a href="https://www.youtube.com/watch?v=8dzh9byoDo4"><Link> "Micro Frontends with React & Redux" talk </Link></a> 
+      about the resulting architecutre and learnings at Dev Day 2018 Berlin.</p>
     <p>I strongly believe that the IT industry should be a diverse and fair place, so
       I have been involved in organising a community for women in IT 
       <a href="https://www.meetup.com/de-DE/Geek-Girls-Carrots-Germany/"><Link> Geek Girls Carrots </Link></a> and 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/PageLayout'
 const IndexPage = () => (
   <Layout>
     <h1>Hello, I am Alexandra.</h1>
-    <p>I am a full-stack developer.</p>
+    <p>I am a senior software developer.</p>
   </Layout>
 )
 
