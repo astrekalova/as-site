5b4689257436761fc5a72be47fd0f6b07103d2a7
Alexandra <strekalo@gmail.com>
2019-01-18 22:36:55 +0100

    embed video in the blog post
    Signed-off-by: Alexandra <strekalo@gmail.com>

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -4,11 +4,13 @@ import Layout from '../components/PageLayout'
 
 const Post = ({ data: { prismicBlogpost } }) => {
   const { data } = prismicBlogpost
+  const embedUrl = data.embedded_link.embed_url.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
   return (
     <Layout>
       <React.Fragment>
         <h1>{data.title.text}</h1>
         <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
+        <iframe width="560" height="315" src={embedUrl} frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </React.Fragment>
     </Layout>
   )
@@ -26,6 +28,9 @@ export const pageQuery = graphql`
         }
         content {
           html
+        }
+        embedded_link {
+          embed_url
         }
       }
     }
