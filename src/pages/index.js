d92bb8e6cf80d252dcb1cf987090ab2ad145ea4a
Alexandra <strekalo@gmail.com>
2019-03-17 23:57:25 +0100

    adjust animations
    Signed-off-by: Alexandra <strekalo@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,26 +9,26 @@ const IndexPage = () => (
   <>
     <UpDown from="0" to="30px">
       <SVG icon="triangle" width="10rem" stroke={blue} left="15%" top="50%" />
-      <SVG icon="hexa" hiddenMobile width="12rem" stroke={green} left="60%" top="25%" />
+      <SVG icon="hexa" hiddenMobile width="12rem" stroke={green} left="55%" top="25%" />
       <SVG icon="box" width="1.5rem" stroke={greyDarker} left="60%" top="15%" />
     </UpDown>
     <UpDownWide>
-      <SVG icon="arrowUp" hiddenMobile width="3rem" fill={red} left="80%" top="20%" />
-      <SVG icon="triangle" width="3rem" stroke={white} left="90%" top="50%" />
+      <SVG icon="arrowUp" hiddenMobile width="3rem" fill={red} left="80%" top="30%" />
+      <SVG icon="triangle" width="3rem" stroke={white} left="88%" top="50%" />
       <SVG icon="triangle" width="2rem" stroke={greyDarkest} left="35%" top="50%" />
       <SVG icon="upDown" hiddenMobile width="2rem" fill={greyDarkest} left="90%" top="10%" />
     </UpDownWide>
-    <SVG icon="circle" hiddenMobile width="6rem" fill={greyDarker} left="2%" top="70%" />
-    <SVG icon="circle" width="1.5rem" fill={greyDarkest} left="2%" top="20%" />
-    <SVG icon="circle" width="3rem" fill={greyDarker} left="50%" top="60%" />
-    <SVG icon="upDown" width="2rem" fill={greyDarkest} left="95%" top="90%" />
-    <SVG icon="upDown" hiddenMobile width="3rem" fill={greyDarker} left="40%" top="75%" />
+    <SVG icon="circle" hiddenMobile width="6rem" fill={greyDarker} left="1%" top="70%" />
+    <SVG icon="circle" width="1.5rem" fill={greyDarkest} left="30%" top="12%" />
+    <SVG icon="circle" width="3rem" fill={greyDarker} left="65%" top="70%" />
+    <SVG icon="upDown" width="2rem" fill={greyDarkest} left="95%" top="85%" />
+    <SVG icon="upDown" hiddenMobile width="3rem" fill={greyDarker} left="45%" top="70%" />
     <Rotate>
       <SVG icon="triangle" width="2rem" stroke={white} />
     </Rotate>
     <SVG icon="box" width="1.5rem" fill={greyDarkest} />
-    <SVG icon="hexa" width="4rem" stroke={greyDarker} left="10%" top="50%" />
-    <SVG icon="hexa" width="2rem" stroke={greyDarker} left="80%" top="70%" />
+    <SVG icon="hexa" width="4rem" stroke={greyDarker} left="6%" top="50%" />
+    <SVG icon="hexa" width="2rem" stroke={greyDarker} left="80%" top="80%" />
     <Layout>
       <h1>Hello, I am Alexandra.</h1>
       <p>I am a senior software developer.</p>
