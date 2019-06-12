653d3f4cb22763c5615193ca5a74bca1861edcb7
Angel <angel@Clementine.local>
2019-06-12 11:57:14 +0300

    use react-social-icons instead of font awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,59 @@
-import React from 'react'
-import styles from "./Footer.module.css"
+import React from 'react';
+import styled from 'styled-components';
+import media from './Breakpoints';
+import { SocialIcon } from 'react-social-icons';
 
-export default () => (
-<footer>
-    <h3 className={styles.subheader}>You can find me here</h3>
-    <ul className={styles.socialLinks}>
-      <li className={styles.socialLink} key='twitter'>
-          <a
-            href='https://twitter.com/astrekalova'
-            className={`${styles.socialLinkIcon} ${styles.twitter}`}
-          >
-            <i className={`fa fa-twitter fa-lg`}></i>
-          </a>
-      </li>
-      <li className={styles.socialLink} key='github'>
-          <a
-            href='https://github.com/astrekalova'
-            className={`${styles.socialLinkIcon} ${styles.github}`}
-          >
-            <i className={`fa fa-github fa-lg`}></i>
-          </a>
-      </li>
-      <li className={styles.socialLink} key='linkedin'>
-          <a
-            href='https://www.linkedin.com/in/alexandra-strekalova-a1a78aa6'
-            className={`${styles.socialLinkIcon} ${styles.linkedin}`}
-          >
-            <i className={`fa fa-linkedin fa-lg`}></i>
-          </a>
-      </li>
-    </ul>
-</footer>)
+const FooterLinks = [
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/astrekalova',
+    color: '#00aced',
+  },
+  {
+    name: 'Github',
+    link: 'https://github.com/astrekalova',
+    color: '#4183c4',
+  },
+  {
+    name: 'linkedin',
+    link: 'https://www.linkedin.com/in/alexandra-strekalova-a1a78aa6',
+    color: '#3b5998',
+  },
+];
+
+const Footer = () => (
+  <footer className="footer">
+    <Subheader>You can find me here</Subheader>
+    <SocialLinks>
+      {FooterLinks.map(link => (
+        <SocialLink key={link.name}>
+          <SocialIcon url={link.link} bgColor={link.color}/>         
+        </SocialLink>
+      ))}
+    </SocialLinks>
+  </footer>
+);
+
+export default Footer;
+
+const Subheader = styled.h3`
+  text-transform: uppercase;
+  font-weight: 900;
+  margin-bottom: 18px;
+  letter-spacing: 5px;
+  text-align: center;
+  font-size: 1.17em;
+`;
+
+const SocialLinks = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  margin-bottom: 1em;
+`;
+
+const SocialLink = styled.li`
+  display: inline;
+  margin-right: 1em;
+`;
